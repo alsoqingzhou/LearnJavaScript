1773b7d1b66f308723c787acd33d009c469c4368
unknown <alsoqingzhou@gmail.com>
2020-08-01 13:19:33 +0800

    add form id and finish project framwork

--- form-validator/script.js
+++ form-validator/script.js
@@ -1,18 +1,17 @@
-function test(person) {
-    person.age = 30
 
-    person = {
-        name: 'aaa',
-        age: 23
-    }
-    return person
-}
 
-const p1 = {
-    name: 'also',
-    age: 11
-}
-const p2 = test(p1)
+//show input error message
 
-console.log(p1)
-console.log(p2)
+//show success outline
+
+//check email is valid
+
+//check required fields
+
+//check input length
+
+//check password match
+
+//get field name
+
+//event listeners
